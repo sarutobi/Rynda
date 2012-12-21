94297d8fa360fb2274f76a2f89f1aec29dc733ac
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2012-12-21 22:51:42 +0400

    Paginator mixin allows build paginator line

--- core/mixins.py
+++ core/mixins.py
@@ -7,10 +7,52 @@ class SubdomainContextMixin(object):
     '''Subdomain context mixin'''
 
     def get_context_data(self, **kwargs):
-        print ('!!!')
         context = super(SubdomainContextMixin, self).get_context_data(**kwargs)
         sc = subdomains_context(self.request)
         for key in sc.keys():
             context[key] = sc[key]
         return context
 
+class PaginatorMixin(object):
+    '''Paginator line mixin. Best use with list-based mixins'''
+
+    def paginator(num_pages, page = 1, adj_pages = 2, outside_range = 3):
+        page = int(page)
+        num_pages = int(num_pages)
+        if page > num_pages:
+            page = num_pages
+
+        if page < 1:
+            page = 1
+        has_prev = has_next = False
+        if num_pages > 1:
+            if page > 1:
+                has_prev = True
+            if page < num_pages:
+                has_next = True
+        #Количество страниц для отображения меньше чем количество 
+        #страниц в пейджере
+        if (outside_range + adj_pages + 1 + adj_pages + outside_range)\
+            >= num_pages:
+            return {'first':[],'window':[n for n in range(1, num_pages+1)],
+                'last':[],  'has_prev':has_prev, 'has_next':has_next}
+
+        #страница для отображения находится в начальном диапазоне
+        if (outside_range + adj_pages + 1 ) >= page:
+            first = []
+            window = [n for n in range(1, outside_range + 2 + 2 * adj_pages)
+                if n > 0 and n < num_pages]
+            last = [n for n in range(num_pages - outside_range, num_pages+1)]
+        elif (num_pages - outside_range - adj_pages - 1) < page:
+            first = [n for n in range(1, outside_range + 1)]
+            window = [n for n in range(num_pages - outside_range - 2 *
+                adj_pages + 1, num_pages +1)]
+            last = []
+        else:
+            first = [n for n in range(1, outside_range + 1)]
+            last = [n for n in range(num_pages - outside_range + 1,
+                num_pages+1)]
+            window = [n for n in range(page - adj_pages, page + adj_pages +1)
+                if n < num_pages]
+        return {'first':first, 'window':window, 'last':last,
+            'has_prev':has_prev, 'has_next':has_next}
