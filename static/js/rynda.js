66063104f6359ea6b601aba0375df7a59640e771
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2012-12-28 18:46:49 +0400

    Move 'all mesages' ajax request to index page only

--- static/js/rynda.js
+++ static/js/rynda.js
@@ -41,15 +41,3 @@ var map = new L.map('locationMapCanvas',
     }
 );
 
-$.ajax({
-    url: '/api/internal/mapmessages/',
-    success: function(data){
-        _.each(data, function(i){
-            var m = new L.Marker(
-                new L.LatLng(i.lat, i.lon),
-                {title: i.title, icon: new L.Icon({iconUrl: '/static/img/nh_icon.png'}) }
-            );
-            m.addTo(map);
-        });
-    }
-});
