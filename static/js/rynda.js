c259841854109419d3b364ac6ee8da655f1466c9
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2012-12-29 17:36:57 +0400

    Create Rynda closure in javascript
    Create closure for Rynda javascripts.
    Move map-related functions to closure.

--- static/js/rynda.js
+++ static/js/rynda.js
@@ -1,43 +1,72 @@
-(function(){
+var Rynda = (function(){
+    var version = "1.0";
 
-    var Rynda ={
-        widgets:{}
-    };
-
-    Rynda.BaseWidget = {
-        instantiate: function(widget){
-            var w = new Rynda.BaseWidget(widget);
-            _.extend(w, Backbone.Event);
-            Rynda.widgets[widget] = w;
-            return w;
-        }
+    function sameheight(group){
+        var tallest = 0;
+        group.each(function(){
+            var h = $(this).height();
+            if (tallest < h){
+                tallest = h;
+            }
+        });
+        group.height(tallest);
     }
-    window.Rynda = Rynda;
+
+    return {
+        version: version,
+        sameheight: sameheight
+    }
 })();
 
-function sameheight(group){
-    var tallest = 0;
-    group.each(function(){
-        var h = $(this).height();
-        if (tallest < h){
-            tallest = h;
-        }
-    });
-    group.height(tallest);
-}
 
+Rynda.map = (function(){
 
-var cloudmade = new L.TileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    { maxZoom: 18,
-      minZoom: 3,
-      attribution: 'карта &copy; OpenStreetMap, рендер &copy; CloudMade'
+    var map;
+    
+
+    function createMap(domElement){
+       var cloudmade = new L.TileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            { maxZoom: 18,
+              minZoom: 3,
+              attribution: 'карта &copy; OpenStreetMap, рендер &copy; CloudMade'
+            }
+        );
+
+        this.map = new L.map(domElement,
+            { center: new L.LatLng(64,95),
+              zoom: 4,
+              layers:[cloudmade]
+            }
+        );
+
     }
-);
-var map = new L.map('locationMapCanvas',
-    { center: new L.LatLng(64,95),
-      zoom: 4,
-      layers:[cloudmade]
+
+    function fetch(){
+        var that = this;
+        var icons = {
+            '1' : '/static/img/nh_icon.png',
+            '2' : '/static/img/wh_icon.png',
+            '3' : '/static/img/nh_icon.png'
+        };
+        $.ajax({
+            url: '/api/internal/mapmessages/',
+            success: function(data){
+                _.each(data, function(i){
+                var m = new L.Marker(
+                    new L.LatLng(i.lat, i.lon),
+                    {title: i.title, icon: new L.Icon({iconUrl: icons[i.messageType]}) }
+                );
+                m.addTo(that.map);
+                });
+            }
+        });
     }
-);
+
+    return  {
+        fetch: fetch,
+        createMap: createMap
+    }
+})();
+
 
