f0f1cd68a7f7da7276dc8bb1ffab567997d28dad
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-01-11 21:46:05 +0400

    Fix autoincrement id for user profile

---
+++ users/migrations/0002_auto__chg_field_users_id.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Users.id'
+        db.delete_column('users_users', 'id')
+        db.add_column('users_users', 'id', self.gf('django.db.models.fields.AutoField')(primary_key=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Users.id'
+        db.alter_column('users_users', 'id', self.gf('django.db.models.fields.IntegerField')(primary_key=True, db_column='id'))
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'users.users': {
+            'Meta': {'ordering': "['user']", 'object_name': 'Users'},
+            'about_me': ('django.db.models.fields.TextField', [], {}),
+            'activCode': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'db_column': "'activation_code'"}),
+            'birthday': ('django.db.models.fields.DateField', [], {}),
+            'flags': ('django.db.models.fields.IntegerField', [], {'default': '0', 'db_column': "'flags'"}),
+            'forgotCode': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'db_column': "'forgotten_password_code'"}),
+            'forgotten_time': ('django.db.models.fields.IntegerField', [], {'db_column': "'forgotten_password_time'"}),
+            'gender': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ipAddr': ('django.db.models.fields.CharField', [], {'max_length': '16', 'db_column': "'ip_address'"}),
+            'my_photo': ('django.db.models.fields.IntegerField', [], {}),
+            'phones': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'ref_type': ('django.db.models.fields.IntegerField', [], {'default': '0', 'db_column': "'ref_type'"}),
+            'rememberCode': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'db_column': "'remember_code'"}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        }
+    }
+
+    complete_apps = ['users']
--- users/models.py
+++ users/models.py
@@ -4,42 +4,42 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-
-class Group(models.Model):
-    class Meta():
-        db_table = 'groups'
-        ordering = ['id']
-
-    id = models.IntegerField(db_column = 'id', primary_key = True)
-    name = models.CharField(max_length = 40, db_column = 'name')
-    description = models.CharField(max_length = 100, db_column = 'description')
-
-    def __unicode__(self):
-        return self.name
+#class Group(models.Model):
+#    class Meta():
+#        db_table = 'groups'
+#        ordering = ['id']
+#
+#    id = models.IntegerField(db_column = 'id', primary_key = True)
+#    name = models.CharField(max_length = 40, db_column = 'name')
+#    description = models.CharField(max_length = 100, db_column = 'description')
+#
+#    def __unicode__(self):
+#        return self.name
 
 class Users(models.Model):
     '''Описание одного пользователя соцсети Рында'''
     class Meta():
-        db_table = 'users'
-        ordering = ['created']
+#        db_table = 'users'
+        ordering = ['user']
 
-    id = models.IntegerField(db_column = 'id', primary_key = True)
+    #id = models.AutoField(db_column = 'id', primary_key = True)
     user = models.OneToOneField(User)
-    groupId = models.ForeignKey(Group, db_column = 'group_id')
+#    groupId = models.ForeignKey(Group, db_column = 'group_id')
     ipAddr = models.CharField(max_length = 16, db_column = 'ip_address')
-    username = models.CharField(max_length = 15, db_column = 'username')
-    password = models.CharField(max_length = 40, db_column = 'password')
-    salt = models.CharField(max_length = 40, db_column = 'salt', null = True)
-    email = models.EmailField(max_length = 100, db_column = 'email')
+#    username = models.CharField(max_length = 15, db_column = 'username')
+#    password = models.CharField(max_length = 40, db_column = 'password')
+#    salt = models.CharField(max_length = 40, db_column = 'salt', null = True)
+#    email = models.EmailField(max_length = 100, db_column = 'email')
     activCode = models.CharField(max_length = 40, db_column = 'activation_code', null = True)
     forgotCode = models.CharField(max_length = 40, db_column = 'forgotten_password_code', null = True)
     rememberCode = models.CharField(max_length = 40, db_column = 'remember_code', null = True)
-    created = models.IntegerField(db_column = 'created_on')
-    lastLogin = models.IntegerField(db_column = 'last_login', null = True)
-    active = models.NullBooleanField(db_column = 'active', null = True)
-    first_name = models.CharField(max_length=50, db_column='first_name')
-    last_name = models.CharField(max_length=50, db_column='last_name')
+#    created = models.IntegerField(db_column = 'created_on')
+#    lastLogin = models.IntegerField(db_column = 'last_login', null = True)
+#    active = models.NullBooleanField(db_column = 'active', null = True)
+#    first_name = models.CharField(max_length=50, db_column='first_name')
+#    last_name = models.CharField(max_length=50, db_column='last_name')
     forgotten_time = models.IntegerField(db_column='forgotten_password_time')
     ref_type = models.IntegerField(db_column='ref_type', default=0)
     flags = models.IntegerField(db_column='flags', default=0)
@@ -48,32 +48,38 @@ class Users(models.Model):
     my_photo = models.IntegerField()
     birthday = models.DateField()
     gender = models.IntegerField(default=0)
-    ip_address = models.CharField(max_length=20)
 
-    def __unicode__(self):
-        return self.username
+#    def __unicode__(self):
+#        return self.username
 
-    def firstName(self):
-        return self.first_name
+#    def firstName(self):
+#        return self.first_name
 
-    def lastName(self):
-        return self.last_name
+#    def lastName(self):
+#        return self.last_name
 
-    def last_login(self):
-        return datetime.fromtimestamp(self.lastLogin)
+#    def last_login(self):
+#        return datetime.fromtimestamp(self.lastLogin)
 
-    def registered(self):
-        return datetime.fromtimestamp(self.created)
+#    def registered(self):
+#        return datetime.fromtimestamp(self.created)
 
-class MetaUser(models.Model):
-    class Meta():
-        db_table = 'meta'
+#def create_user_profile(sender, instance, created, **kwargs):
+#    if created:
+#        Users.objects.create(user=instance)
 
-    id = models.IntegerField(db_column = 'id', primary_key = True)
-    user = models.OneToOneField(Users, db_column = 'user_id')
-    firstName = models.CharField(max_length = 50, db_column = 'first_name')
-    lastName = models.CharField(max_length = 50, db_column = 'last_name')
-    flags = models.IntegerField(db_column = 'flags')
+#post_save.connect(create_user_profile, sender=User)
 
-    def __unicode__(self):
-        return "%s %s" % (self.lastName, self.firstName)
+
+#class MetaUser(models.Model):
+#    class Meta():
+#        db_table = 'meta'
+#
+#    id = models.IntegerField(db_column = 'id', primary_key = True)
+#    user = models.OneToOneField(Users, db_column = 'user_id')
+#    firstName = models.CharField(max_length = 50, db_column = 'first_name')
+#    lastName = models.CharField(max_length = 50, db_column = 'last_name')
+#    flags = models.IntegerField(db_column = 'flags')
+#
+#    def __unicode__(self):
+#        return "%s %s" % (self.lastName, self.firstName)
