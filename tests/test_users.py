b5188df092895be3d887db434914ee53a9110720
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-01-26 14:16:44 +0400

    Add user-related tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,6 +2,21 @@
 
 import unittest
 
+from factories import UserFactory
+
 class UserTest(unittest.TestCase):
+    '''User-specific tests'''
+    def setUp(self):
+        self.user = UserFactory.build()
+
+    def tearDown(self):
+        self.user = None
+
     def test_user(self):
-        self.assertEqual('a', 'b')
+        self.assertNotEqual(None, self.user)
+        self.assertEqual('Boy', self.user.first_name)
+        self.assertEqual('Factory', self.user.last_name)
+        self.assertEqual('boy_factory@example.com', self.user.email)
+
+    def test_user_generator(self):
+        pass
