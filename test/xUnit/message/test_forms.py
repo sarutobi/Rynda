bd35720b4afe6a63b1fdc2e34290bb0d1a609a32
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-01-30 22:22:37 +0400

    Add test form validation

--- test/xUnit/message/test_forms.py
+++ test/xUnit/message/test_forms.py
@@ -3,15 +3,30 @@
 import unittest
 
 from message.forms import SimpleRequestForm
+from test.utils import lorem_ipsum, generate_string
+from test.factories import UserFactory
 
 
 class TestSimpleRequestForm(unittest.TestCase):
 
     def setUp(self):
         self.form = SimpleRequestForm()
+        self.user = UserFactory.build()
 
     def tearDown(self):
         self.form = None
+        self.user = None
 
     def test_form_type(self):
         self.assertEqual(1, self.form.fields['messageType'].initial)
+
+    def test_send_data(self):
+        data = {
+            'message': lorem_ipsum(),
+            'contact_first_name': self.user.first_name,
+            'contact_last_name': self.user.last_name,
+            'contact_mail': self.user.email
+        }
+        form = SimpleRequestForm(data)
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.is_valid())
