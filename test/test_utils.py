d68aab51ecf303987bbf2aca3f5894d0acbd1e87
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-01-30 20:40:24 +0400

    Add 'lorem_ipsum' text generator

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from utils import generate_string
+from test.utils import generate_string, lorem_ipsum
 
 class TestGenerateString(unittest.TestCase):
     '''Test string generator function'''
@@ -10,3 +10,10 @@ class TestGenerateString(unittest.TestCase):
         gen_str = generate_string()
         self.assertEqual(6, len(gen_str))
         self.assertEqual(gen_str, gen_str.lower())
+
+    def test_lorem_ipsum(self):
+        lorem = lorem_ipsum()
+        self.assertIsNotNone(lorem)
+        lorem_len = len(lorem.split(" "))
+        self.assertTrue(lorem_len >= 20)
+        self.assertTrue(lorem_len <= 60)
--- test/utils.py
+++ test/utils.py
@@ -4,3 +4,11 @@ import random
 
 def generate_string(str_len=6, src=string.ascii_lowercase):
     return "".join(random.choice(src) for x in xrange(str_len))
+
+def lorem_ipsum():
+    words_count = random.randint(20, 50)
+    lorem = list([])
+    for i in xrange(words_count):
+        word_length = random.randint(4, 8)
+        lorem.append(generate_string(str_len=word_length))
+    return " ".join(lorem)
