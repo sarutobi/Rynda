6f7dba3beccca655b84879ccd0f3071d15536b2f
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-01-31 21:06:49 +0400

    Add word_count parameter for lorem_ipsum generator

--- test/utils.py
+++ test/utils.py
@@ -2,11 +2,12 @@
 import string
 import random
 
+
 def generate_string(str_len=6, src=string.ascii_lowercase):
     return "".join(random.choice(src) for x in xrange(str_len))
 
-def lorem_ipsum():
-    words_count = random.randint(20, 50)
+
+def lorem_ipsum(words_count=30):
     lorem = list([])
     for i in xrange(words_count):
         word_length = random.randint(4, 8)
