608b84a4dcc2ba78c140077ed3bec937f8133117
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-02-01 19:02:11 +0400

    add username field to UserFactory

--- test/factories.py
+++ test/factories.py
@@ -17,6 +17,7 @@ class UserFactory(factory.Factory):
     email = factory.LazyAttribute(
         lambda a:
         "{0}_{1}@example.com".format(a.first_name, a.last_name).lower())
+    username = factory.Sequence(lambda n: "username_%s" % n)
 
 
 class MessageFactory(factory.Factory):
