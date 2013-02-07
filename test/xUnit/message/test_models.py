88ee0441ae0257e47090d018a930d1af1bb69582
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-02-07 20:16:48 +0400

    Rewritting message tests

--- test/xUnit/message/test_models.py
+++ test/xUnit/message/test_models.py
@@ -4,7 +4,7 @@ import unittest
 
 from django.core.exceptions import ValidationError
 
-from factories import MessageFactory
+from factories import MessageFactory, UserFactory
 
 
 class TestMessage(unittest.TestCase):
@@ -12,42 +12,43 @@ class TestMessage(unittest.TestCase):
     Test messages
     '''
     def setUp(self):
-        self.message = MessageFactory()
+        self.user = UserFactory()
+        self.message = MessageFactory.build()
+        self.message.user = self.user.pk
 
     def tearDown(self):
-        self.message.delete()
+        self.user.delete()
         self.message = None
 
     def test_message(self):
+        self.message.save()
         self.assertIsNotNone(self.message.pk)
 
-    def test_no_message_contacts(self):
-        m = MessageFactory.build(contact_phone=None)
-        m.contact_mail = None
+    def catch_wrong_data(self):
         with self.assertRaises(ValidationError):
-            m.save()
-            m.delete()
-        m = None
+            self.message.save()
+            self.message.delete()
+
+    def test_no_message_contacts(self):
+        self.message.contact_phone = None
+        self.message.contact_mail = None
+        self.catch_wrong_data()
 
     def test_invalid_email(self):
-        m = MessageFactory.build(contact_mail='notamail')
-        m.contact_mail = 'notamail'
-        with self.assertRaises(ValidationError):
-            m.save()
-            m.delete()
-        m = None
+        self.message.contact_mail = 'notamail'
+        self.catch_wrong_data()
 
     def test_phone_contact(self):
-        m = MessageFactory.build()
-        m.contact_mail = ''
-        m.save()
-        self.assertIsNotNone(m.pk)
-        m.delete()
+        self.message.contact_mail = ''
+        self.message.save()
+        self.assertIsNotNone(self.message.pk)
+        self.message.delete()
 
     def test_email_contact(self):
-        m = MessageFactory.build(contact_phone='')
-        m.save()
-        self.assertIsNotNone(m.pk)
+        self.message.contact_phone = ''
+        self.message.save()
+        self.assertIsNotNone(self.message.pk)
+        self.message.delete()
 
     def test_message_remove(self):
         self.message.remove()
@@ -59,3 +60,16 @@ class TestMessage(unittest.TestCase):
         self.message.restore()
         self.assertFalse(self.message.is_removed())
 
+
+class TestUserMessage(unittest.TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+
+    def tearDown(self):
+        self.user.delete()
+        self.user = None
+
+    def test_user_message(self):
+        msg = MessageFactory(user=self.user.pk)
+        self.assertIsNotNone(msg)
+        self.assertEqual(self.user.pk, msg.user)
