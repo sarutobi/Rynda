839d950ea69c55d1364a6529489a72bcee5bf19b
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-02-07 20:18:05 +0400

    Factories fix
    Fix user names in User factory.
    Add user link to message factory.

--- test/factories.py
+++ test/factories.py
@@ -27,8 +27,8 @@ class MessageFactory(factory.Factory):
     FACTORY_FOR = Message
 
     message = lorem_ipsum()
-    contact_first_name = generate_string()
-    contact_last_name = generate_string()
+    contact_first_name = factory.Sequence(lambda n: "user_%s" % n)
+    contact_last_name = factory.Sequence(lambda n: "name_%s" % n)
     contact_mail = factory.LazyAttribute(
         lambda a:
         "{0}_{1}@example.com".format(
@@ -36,3 +36,4 @@ class MessageFactory(factory.Factory):
             a.contact_last_name
         ).lower())
     contact_phone = generate_string(str_len=10, src=string.digits)
+    user = factory.SubFactory(UserFactory)
