c78aa5abc18dda674f607ead5af59ddb4a879ed4
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-02-08 23:40:13 +0400

    Move location from core to geozones

--- geozones/models.py
+++ geozones/models.py
@@ -7,11 +7,13 @@ from django.utils.translation import ugettext_lazy as _
 
 class Region(models.Model):
     '''
+    Region
+    ======
     Common regional zones. All messages can be grouped by this territorial
     cluster.
-    TODO: use django-mptt
-    TODO: make nested regions
-    TODO: link message to nested regions
+    * TODO: use django-mptt
+    * TODO: make nested regions
+    * TODO: link message to nested regions
     '''
     class Meta():
         ordering = ['order']
@@ -29,4 +31,30 @@ class Region(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class Location(models.Model):
+    '''
+    Geolocation
+    ===========
+    This data represent POI(Point Of Interest).
+    Object contain small piece of data:
+    * Geocoordinates - latitude and longitude
+    * Description - textual name of POI or it's human-readable address
+    * Optional link to georegion. If this link exists
+    '''
+    # Geocoordinates
+    latitude = models.FloatField()
+    longitude = models.FloatField(db_column='longitude')
+    # Optional link for region
+    region = models.ForeignKey(
+        Region,
+        verbose_name=_("region"),
+        null=True, blank=True)
+    # Short description or address
+    description = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return u'%f %f' % (self.latitude, self.longitude)
+
 
