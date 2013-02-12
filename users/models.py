5eba5c2cecda285834ce799fe5c4f56f2ff844d3
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-02-12 19:51:14 +0400

    Add self-activation validator class

--- users/models.py
+++ users/models.py
@@ -1,37 +1,71 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
+import random
+import base64
+import hashlib
+import string
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
 from templated_emails.utils import send_templated_email
 
 from core.backends import IonAuth
 
-#class Group(models.Model):
-#    class Meta():
-#        db_table = 'groups'
-#        ordering = ['id']
-#
-#    id = models.IntegerField(db_column = 'id', primary_key = True)
-#    name = models.CharField(max_length = 40, db_column = 'name')
-#    description = models.CharField(max_length = 100, db_column = 'description')
-#
-#    def __unicode__(self):
-#        return self.name
+
+class UserAuthCode(object):
+    def __init__(self, secret, salt_len=8, hash=hashlib.sha256):
+        self.secret = secret
+        self.salt_len = salt_len
+        self.hash = hash
+
+    def salt(self):
+        s = [random.choice(string.letters + string.digits)
+             for i in xrange(self.salt_len)]
+        return "".join(s)
+
+    def digest(self, user, salt):
+        # Use username, email and date_joined to generate digest
+        auth_message = ''.join((self.secret, user.username, user.email,
+                               str(user.date_joined), salt))
+        md = self.hash()
+        md.update(auth_message)
+
+        return base64.urlsafe_b64encode(md.digest()).rstrip('=')
+
+    def auth_code(self, user):
+        salt = self.salt()
+        digest = self.digest(user, salt)
+
+        return "%s%s" % (salt, digest)
+
+    def is_valid(self, user, auth_code):
+        #import pdb; pdb.set_trace()
+        salt = auth_code[:self.salt_len]
+        digest = auth_code[self.salt_len:]
+
+        # CAVEAT: Make sure UserAuthCode cannot be used to reactivate locked
+        # profiles.
+        if user.last_login >= user.date_joined:
+            return False
+
+        return digest == self.digest(user, salt)
+
 
 class Users(models.Model):
-    '''Описание одного пользователя соцсети Рында'''
+    '''
+    User profile
+    '''
     class Meta():
         ordering = ['user']
 
     user = models.OneToOneField(User)
 #    ipAddr = models.CharField(max_length = 16, db_column = 'ip_address')
 #    email = models.EmailField(max_length = 100, db_column = 'email')
-    activCode = models.CharField(max_length = 40,
-        db_column = 'activation_code', null = True)
+    activCode = models.CharField(
+        max_length=40,
+        db_column='activation_code',
+        null=True)
     forgotCode = models.CharField(max_length = 40,
         db_column = 'forgotten_password_code', null = True)
     rememberCode = models.CharField(max_length = 40,
@@ -46,17 +80,17 @@ class Users(models.Model):
     birthday = models.DateField(null=True)
     gender = models.IntegerField(default=0)
 
-#    def __unicode__(self):
-#        return self.username
+    def __unicode__(self):
+        return "Profile for %s" % self.user.username
 
 
 def create_new_user(first_name, last_name, password, email):
     auth = IonAuth()
     user = User(
-        first_name = first_name,
-        last_name = last_name,
-        email = email,
-        username = email,
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        username=email,
         is_staff=False,
         is_active=False
     )
@@ -64,14 +98,14 @@ def create_new_user(first_name, last_name, password, email):
     user.save()
     profile = Users.objects.create(
         user=user,
-        activCode = auth.generate_code()
+        activCode=auth.generate_code()
     )
 
 
 def notify_new_user(user, code):
-    send_templated_email([user], 'emails/registration_confirm',
-            {'user': user, 'site_url': self.request.META['SERVER_NAME'],
-             'activation_code': code,
-            }
-        )
+    send_templated_email(
+        [user], 'emails/registration_confirm',
+        {'user': user, 'site_url': self.request.META['SERVER_NAME'],
+         'activation_code': code, }
+    )
 
