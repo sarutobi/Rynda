0a53e935d27c19dca0555e832b9d52a298862fec
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-02-13 21:24:27 +0400

    Improved location import

--- message/models.py
+++ message/models.py
@@ -8,7 +8,8 @@ from django.utils.translation import ugettext_lazy as _
 
 from django.contrib.auth.models import User
 
-from core.models import Location, Category, Subdomain
+from core.models import Category, Subdomain
+from geozones.models import Location
 
 
 class ApprovedMessages(models.Manager):
