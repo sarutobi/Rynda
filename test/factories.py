6d80ab98cb163366e7795992054ef8703f267f33
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-02-13 19:10:20 +0400

    Moved message factory to application

--- test/factories.py
+++ test/factories.py
@@ -1,13 +1,8 @@
 # coding: utf-8
 
-import string
 import factory
-import random
 
 from django.contrib.auth.models import User
-
-from message.models import Message, MessageType
-from test.utils import generate_string, lorem_ipsum
 
 
 class UserFactory(factory.Factory):
@@ -19,24 +14,3 @@ class UserFactory(factory.Factory):
         lambda a:
         "{0}_{1}@example.com".format(a.first_name, a.last_name).lower())
     username = factory.Sequence(lambda n: "username_%s" % n)
-
-
-class MessageFactory(factory.Factory):
-    '''
-    Factory for messages
-    '''
-    FACTORY_FOR = Message
-
-    message = lorem_ipsum()
-    contact_first_name = factory.Sequence(lambda n: "user_%s" % n)
-    contact_last_name = factory.Sequence(lambda n: "name_%s" % n)
-    contact_mail = factory.LazyAttribute(
-        lambda a:
-        "{0}_{1}@example.com".format(
-            a.contact_first_name,
-            a.contact_last_name
-        ).lower())
-    contact_phone = generate_string(str_len=10, src=string.digits)
-    user = factory.SubFactory(UserFactory)
-    messageType = random.randint(
-        MessageType.TYPE_REQUEST, MessageType.TYPE_INFO)
