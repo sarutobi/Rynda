7ebcd355097ea432f006c1693b23c529fca35915
Valery A. Ilychev <ilychev@gmail.com>
2013-04-29 16:42:13 +0400

    Added admin message form

--- message/admin.py
+++ message/admin.py
@@ -3,9 +3,16 @@
 from django.contrib import admin
 
 from .models import Message
+from .forms import MessageForm
 
 
 class MessageAdmin(admin.ModelAdmin):
+    class Media:
+        js = (
+            'js/libs/jquery-1.8.1.min.js',
+            'js/libs/leaflet.js',
+            'js/plugins.js',
+            'js/rynda.js',)
     list_display = (
         'pk',
         'title',
@@ -16,5 +23,20 @@ class MessageAdmin(admin.ModelAdmin):
     )
     list_display_links = ('pk', 'title')
     list_filter = ('status',)
+    form = MessageForm
+
+    fieldsets = (
+        ('Message', {
+            'fields': ('messageType', 'title', 'message'),
+        }),
+        ('Author', {
+            'fields': ('contact_first_name', 'contact_last_name',
+                       'contact_mail', 'contact_phone')
+        }),
+        ('Geoposition', {
+            'fields': ('georegion', 'address', 'location')
+        })
+
+    )
 
 admin.site.register(Message, MessageAdmin)
