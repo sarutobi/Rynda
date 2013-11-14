947cdc71d9bf80883ddc514afff0cbea52fa79cf
Valery A. Ilychev <ilychev@gmail.com>
2013-11-14 08:32:11 +0400

    Improved user factory to new FactoryBoy abilities

--- test/factories.py
+++ test/factories.py
@@ -1,11 +1,12 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 import factory
+from factory import django
 
 from django.contrib.auth.models import User
 
 
-class UserFactory(factory.Factory):
+class UserFactory(django.DjangoModelFactory):
     FACTORY_FOR = User
 
     first_name = "Boy"
@@ -14,17 +15,7 @@ class UserFactory(factory.Factory):
         lambda a:
         "{0}_{1}@mail.ru".format(a.first_name, a.last_name).lower())
     username = factory.Sequence(lambda n: "username_%s" % n)
-    password = '123'
-    is_active = False
+    password = factory.PostGenerationMethodCall('set_password', '123')
+    is_active = True
     is_staff = False
     is_superuser = False
-
-    @classmethod
-    def _prepare(cls, create, **kwargs):
-        password = kwargs.pop("password", None)
-        user = super(UserFactory, cls)._prepare(create, **kwargs)
-        if password:
-            user.set_password(password)
-        if create:
-            user.save()
-        return user
