0b78f6336f470afaa792859fc41c2af99d215610
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-11-25 23:03:28 +0400

    Added stage command to local commands

--- fabfile/local.py
+++ fabfile/local.py
@@ -21,3 +21,9 @@ def requirements(settings='base'):
     req = REQUIREMENT_SET.get(settings, REQUIREMENT_SET.get('base'))
     with prefix(VIRT_ACTIVATE), prefix(VIRT_COMMAND):
         local('pip install -r %s' % req)
+
+
+@task
+def stage():
+    with prefix(LOCAL_VIRT_ACTIVATE), prefix(VIRT_COMMAND):
+        local("./manage.py runserver 0.0.0.0:8000 --settings=Rynda.settings.local_stage")
