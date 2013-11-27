e88a400a384543514026707211473e72f7703879
Valery A. Ilychev <ilychev@gmail.com>
2013-11-27 16:59:52 +0400

    Forms optimisation

--- message/forms.py
+++ message/forms.py
@@ -5,26 +5,39 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 
 from core.widgets import CategoryTree
-from geozones.forms import LocationField
 from geozones.widgets import GeolocationWidget
 from message.models import Message
 
 
+class VirtualMessageFormMixin(forms.ModelForm):
+
+    """ Data for virtual message. """
+
+    def __init__(self, *args, **kwargs):
+        super(VirtualMessageFormMixin, self).__init__(*args, **kwargs)
+        self.fields['is_virtual'] = forms.BooleanField(default=True)
+
+    def clean_isvirtual(self):
+        return True
+
+    def clean_location(self):
+        return None
+
+
 class MessageForm(forms.ModelForm):
-    ''' Base message form'''
+
+    """ Base message form"""
+
     class Meta:
         model = Message
         fields = (
             'title', 'message', 'messageType', 'subdomain',
             'category',
             'is_anonymous', 'allow_feedback',
-            'location', )
+        )
         widgets = {
-            'location': GeolocationWidget(),
             'category': CategoryTree(),
         }
-
-    location = LocationField(required=False)
 
     def clean_messageType(self):
         test = self.cleaned_data['messageType']
@@ -32,32 +45,17 @@ class MessageForm(forms.ModelForm):
             raise ValidationError(_("Unknown message type!"))
         return test
 
-    def clean_location(self):
-        location = self.cleaned_data['location']
-        if location is None:
-            return ""
-        elif isinstance(location, list):
-            return 'POINT(%f %f)' % (location[0], location[1])
-        return location
-
-    def save(self, *args, **kwargs):
-        return super(MessageForm, self).save(*args, **kwargs)
-
 
 class UserMessageForm(MessageForm):
     class Meta(MessageForm.Meta):
         widgets = {
             'messageType': forms.HiddenInput(),
-            'location': GeolocationWidget(),
             'category': CategoryTree(),
         }
 
-    def save(self, *args, **kwargs):
-        return super(UserMessageForm, self).save(*args, **kwargs)
-
 
 class RequestForm(UserMessageForm):
-    ''' Simple request form. '''
+    """ Simple request form. """
 
     def __init__(self, *args, **kwargs):
         super(RequestForm, self).__init__(*args, **kwargs)
@@ -68,7 +66,7 @@ class RequestForm(UserMessageForm):
 
 
 class OfferForm(UserMessageForm):
-    ''' Simple offer form. '''
+    """ Simple offer form. """
 
     def __init__(self, *args, **kwargs):
         super(OfferForm, self).__init__(*args, **kwargs)
@@ -79,7 +77,7 @@ class OfferForm(UserMessageForm):
 
 
 class InformationForm(UserMessageForm):
-    ''' Simple information form. '''
+    """ Simple information form. """
 
     def __init__(self, *args, **kwargs):
         super(InformationForm, self).__init__(*args, **kwargs)
@@ -93,10 +91,8 @@ class AdminMessageForm(MessageForm):
     class Meta(MessageForm.Meta):
         widgets = {
             'messageType': forms.Select(),
-            'location': GeolocationWidget(),
             'category': CategoryTree(),
         }
 
     def clean_messageType(self):
         return self.cleaned_data['message_type']
-
