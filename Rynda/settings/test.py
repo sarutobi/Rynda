9afaf48aae9d065f663c8f678919c8ba7a49e4d8
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2014-02-05 19:18:19 +0400

    Изменения в настройках тестов - убран внешний TestRunner.

--- Rynda/settings/test.py
+++ Rynda/settings/test.py
@@ -1,7 +1,5 @@
 # coding: utf-8
 
-import os
-
 from .local import *
 
 DATABASES = {
@@ -14,7 +12,5 @@ DATABASES = {
         'PORT': '',
     }
 }
-#TEST_RUNNER = 'discover_runner.DiscoverRunner'
-#TEST_DISCOVER_TOP_LEVEL = os.path.join(SITE_ROOT, 'test')
-# TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
+
 SOUTH_TESTS_MIGRATE = False
