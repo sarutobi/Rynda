4063a8df80d5b19e11f18dbb91389c8ae9875478
Valery A. Ilychev <ilychev@gmail.com>
2014-02-19 17:21:17 +0400

    Удалены freeze models  в миграции ядра

--- core/migrations/0008_del_models.py
+++ core/migrations/0008_del_models.py
@@ -15,12 +15,4 @@ class Migration(SchemaMigration):
     def backwards(self, orm):
         pass
 
-    models = {
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
+
