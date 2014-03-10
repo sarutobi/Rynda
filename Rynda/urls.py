c11662334585f3375c9a7b7f0a3e0f02a8d38e36
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2014-03-10 17:32:37 +0400

    Оптимизация главного файла url.

--- Rynda/urls.py
+++ Rynda/urls.py
@@ -1,28 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 from users.views import CreateUser
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'RyndaRebuild.views.home', name='home'),
-    # url(r'^RyndaRebuild/', include('RyndaRebuild.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^$', 'message.views.list'),
     url(r'', include('social_auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin-tools/', include('admin_tools.urls')),
+    url(r'^api/', include('api.urls')),
     url(r'^info/', include('core.urls')),
-    url(r'^logout$', 'message.views.logout_view'),
+    # url(r'^logout$', 'message.views.logout_view'),
     url(r'^message/', include('message.urls')),
     url(r'^register$', CreateUser.as_view()),
     url(r'^t/(?P<slug>[a-z_0-9-]+)$', 'message.views.list'),
@@ -33,19 +25,15 @@ urlpatterns = patterns('',
 urlpatterns += patterns('django.contrib.auth.views',
     url(r'^login$', 'login',
         {'template_name': 'login.html'}),
+    url(r'^logout$', 'logout', ),
     url(r'^password/reset$', 'password_reset',
         ),
     url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         'password_reset_confirm',),
     url(r'^password/reset/complete$',
-        'password_reset_complete'),
-    url(r'^password/reset/done',
-        'password_reset_done', ),
+        'password_reset_complete',),
+    url(r'^password/reset/done', 'password_reset_done', name='password_reset_done', ),
 )
 urlpatterns += patterns('core.views',
 
-)
-
-urlpatterns += patterns('',
-    (r'^api/', include('api.urls')),
 )
