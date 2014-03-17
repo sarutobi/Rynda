07d47f5fd12f643b5b87585518db016f6ec0fd54
Valery A. Ilychev <ilychev@gmail.com>
2014-03-17 13:54:49 +0400

    В url пейджера добавлен завершающий слэш.

--- message/urls.py
+++ message/urls.py
@@ -5,7 +5,7 @@ from message.views import MessageView, CreateRequest, MessageList,\
 
 urlpatterns = patterns('',
     url(r'^$', MessageList.as_view(), name="message_list"),
-    url(r'^page/(?P<page>\d+)$', MessageList.as_view()),
+    url(r'^page/(?P<page>\d+)/$', MessageList.as_view()),
     (r'^(?P<pk>\d+)$', MessageView.as_view()),
  #   url(r'^pomogite$', 'message.views.requests'),
  #   url(r'^pomogu$', 'message.views.offer'),
--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ from users.views import UserDetail, UserList
 
 urlpatterns = patterns('users.views',
     url(r'^$', UserList.as_view()),
-    url(r'^page/(?P<page>\d+)$', UserList.as_view()),
+    url(r'^page/(?P<page>\d+)/$', UserList.as_view()),
     url(r'^(?P<pk>\d+)$', UserDetail.as_view()),
     url(r'^activate/(?P<pk>\d+)(?P<key>[a-f0-9]{40})$', 'activate_profile'),
     #url(r'^requestpassword$', ''),
