ac126ee604a205d7fb7ae45d17f10a9765f6fdc8
Valery A. Ilychev <ilychev@gmail.com>
2014-04-02 11:17:58 +0400

    Оптимизация файла geozones/admin.py
    Убрано неиспользуемое описание формы для регионов.

--- geozones/admin.py
+++ geozones/admin.py
@@ -2,20 +2,11 @@
 
 from django.contrib import admin
 from olwidget.admin import GeoModelAdmin
-from olwidget.forms import MapModelForm
 
 from .models import Region
 
 
-class RegionAdminForm(MapModelForm):
-    """ Форма редактирования региона для админки """
-    class Meta:
-        # model = Region
-        template = "geozones/olwidget/admin_olwidget.html"
-
-
 class RegionGeoModel(GeoModelAdmin):
-    # form = RegionAdminForm
     map_template = "geozones/olwidget/admin_olwidget.html"
 
 
