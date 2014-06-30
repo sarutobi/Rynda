1f1ba6ef0a337be368132e077d66dac473571e81
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2014-06-30 22:01:55 +0400

    Генераторы тестовых геоданных перенесены в ядро приложения

--- core/factories.py
+++ core/factories.py
@@ -1,12 +1,37 @@
 # coding: utf-8
 
 import factory
+import random
 
 from factory import django, fuzzy
+from django.contrib.gis.geos import Point, MultiPoint, GeometryCollection
 
 from test.utils import FuzzyText
 from category.models import Category, CategoryGroup
 from .models import Subdomain
+
+
+class FuzzyPoint(fuzzy.BaseFuzzyAttribute):
+    """ Generate fuzzy geojango point """
+    def fuzz(self):
+        lat = random.uniform(-90.0, 90.0)
+        lon = random.uniform(-180.0, 180.0)
+        return Point(lon, lat).wkt
+
+
+class FuzzyMultiPoint(fuzzy.BaseFuzzyAttribute):
+    def fuzz(self):
+        p1 = Point(0, 1)
+        p2 = Point(2, 4)
+        mp = MultiPoint(p1, p2)
+        return mp.wkt
+
+
+class FuzzyGeometryCollection(fuzzy.BaseFuzzyAttribute):
+    def fuzz(self):
+        p1 = Point(1, 2)
+        p2 = Point(3, 4)
+        return GeometryCollection(p1, p2).wkt
 
 
 #class CityFactory(factory.Factory):
--- geozones/factories.py
+++ geozones/factories.py
@@ -1,29 +1,12 @@
 # coding: utf-8
 
 import factory
-
-from factory import django
-from factory.fuzzy import BaseFuzzyAttribute
-
 import random
 
+from factory import django
+
+from core.factories import FuzzyPoint, FuzzyGeometryCollection
 from .models import Location, Region
-
-
-class FuzzyPoint(BaseFuzzyAttribute):
-    """ Generate fuzzy geojango point """
-    def fuzz(self):
-        lat = random.uniform(-90.0, 90.0)
-        lon = random.uniform(-180.0, 180.0)
-        return "POINT(%f %f)" % (lon, lat)
-
-
-class FuzzyPoints(BaseFuzzyAttribute):
-    """ Generate list of geodjango points """
-    def fuzz(self):
-        lat = random.uniform(-90.0, 90.0)
-        lon = random.uniform(-180.0, 180.0)
-        return "GEOMETRYCOLLECTION(POINT(%f %f))" % (lon, lat)
 
 
 class RegionFactory(django.DjangoModelFactory):
@@ -40,6 +23,6 @@ class LocationFactory(django.DjangoModelFactory):
     FACTORY_FOR = Location
 
     name = factory.Sequence(lambda n: "Item %s" % n)
-    coordinates = FuzzyPoints()
+    coordinates = FuzzyGeometryCollection()
     description = factory.Sequence(lambda n: "Location_%s" % n)
     region = factory.SubFactory(RegionFactory)
