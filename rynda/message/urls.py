968d50809e4923e2afdd575f0aa0fa163b2422de
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2014-08-01 21:00:11 +0400

    Optimized message urls

--- rynda/message/urls.py
+++ rynda/message/urls.py
@@ -4,11 +4,11 @@ from message.views import MessageView, CreateRequest, MessageList,\
     CreateOffer
 
 urlpatterns = patterns('',
-    url(r'^$', MessageList.as_view(), name="message_list"),
-    url(r'^page/(?P<page>\d+)/$', MessageList.as_view()),
-    (r'^(?P<pk>\d+)$', MessageView.as_view()),
+    url(r'^$', MessageList.as_view(), name="messages-list"),
+    url(r'^page/(?P<page>\d+)/$', MessageList.as_view(), name='message-list-paged'),
+    url(r'^(?P<pk>\d+)$', MessageView.as_view(), name='message-details'),
  #   url(r'^pomogite$', 'message.views.requests'),
  #   url(r'^pomogu$', 'message.views.offer'),
-    url('^pomogite/dobavit', CreateRequest.as_view(), name='create-request'),
-    ('^pomogu/dobavit', CreateOffer.as_view()),
+    url('^pomogite/dobavit', CreateRequest.as_view(), name='message-create-request'),
+    url('^pomogu/dobavit', CreateOffer.as_view(), name='message-create-offer'),
 )
