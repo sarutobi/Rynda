ad6540fa0c4636761695faf22b4d7c1019c2fdc1
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2014-08-01 20:26:13 +0400

    Organized main urls list

--- rynda/Rynda/urls.py
+++ rynda/Rynda/urls.py
@@ -7,39 +7,40 @@ from users.views import CreateUser
 
 admin.autodiscover()
 
+# Include external apps views
 urlpatterns = patterns('',
-    url(r'^$', 'message.views.list'),
     url(r'', include('social_auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
-    # url(r'^admin-tools/', include('admin_tools.urls')),
-    url(r'^api/', include('api.urls')),
-    # url(r'^logout$', 'message.views.logout_view'),
-    url(r'^message/', include('message.urls')),
-    url(r'^organization/', include('organizations.urls')),
-    url(r'^register$', CreateUser.as_view()),
-    url(r'^t/(?P<slug>[a-z_0-9-]+)$', 'message.views.list'),
-    url(r'^t/(?P<slug>)message/', include('message.urls')),
-    url(r'^user/', include('users.urls')),
 )
 
+# Rynda-related patterns
+urlpatterns += patterns('',
+    url(r'^$', 'message.views.list', name='main-index'),
+    url(r'^api/', include('api.urls')),
+    url(r'^message/', include('message.urls')),
+    url(r'^organization/', include('organizations.urls')),
+    url(r'^user/', include('users.urls')),
+    url(r'^t/(?P<slug>[a-z_0-9-]+)$', 'message.views.list'),
+    url(r'^t/(?P<slug>)message/', include('message.urls')),
+)
+
+# Project description patterns
 urlpatterns += patterns(
     'django.contrib.flatpages.views',
     url(r'info/about/$', 'flatpage', {'url': '/about/'}, name="about_project"),
 )
 
+# Account-related patterns
 urlpatterns += patterns('django.contrib.auth.views',
+    url(r'^register$', CreateUser.as_view(), name='user-creation'),
     url(r'^login$', 'login',
-        {'template_name': 'login.html'}),
-    url(r'^logout$', 'logout', ),
-    url(r'^password/reset$', 'password_reset',
-        ),
+        {'template_name': 'login.html'}, name='user-login'),
+    url(r'^logout$', 'logout', name='user-logout'),
+    url(r'^password/reset$', 'password_reset', name='user-password-reset'),
     url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        'password_reset_confirm',),
+        'password_reset_confirm', name='user-password-reset-confirm'),
     url(r'^password/reset/complete$',
-        'password_reset_complete',),
-    url(r'^password/reset/done', 'password_reset_done', name='password_reset_done', ),
-)
-urlpatterns += patterns('core.views',
-
+        'password_reset_complete', name='user-password-reset-complete'),
+    url(r'^password/reset/done', 'password_reset_done', name='user-password-reset-done', ),
 )
