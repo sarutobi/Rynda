74b3a51ed3bfa6b9e90e1fa56ea65f8b65dd1b98
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2014-08-03 11:51:06 +0400

    Improved user urls.

--- rynda/users/urls.py
+++ rynda/users/urls.py
@@ -1,15 +1,14 @@
 #-*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 from users.views import UserDetail, UserList
 
 
 urlpatterns = patterns('users.views',
-    url(r'^$', UserList.as_view()),
-    url(r'^page/(?P<page>\d+)/$', UserList.as_view()),
-    url(r'^(?P<pk>\d+)$', UserDetail.as_view()),
-    url(r'^activate/(?P<pk>\d+)(?P<key>[a-f0-9]{40})$', 'activate_profile'),
+    url(r'^$', UserList.as_view(), name="user-list"),
+    url(r'^page/(?P<page>\d+)/$', UserList.as_view(), name='user-list'),
+    url(r'^(?P<pk>\d+)$', UserDetail.as_view(), name='user-details'),
+    url(r'^activate/(?P<pk>\d+)(?P<key>[a-f0-9]{40})$', 'activate_profile', name='user-activate-profile'),
     #url(r'^requestpassword$', ''),
 )
-
