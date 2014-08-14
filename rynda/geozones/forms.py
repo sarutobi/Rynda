95ec2362b1126842364136ca3df5a638c8817955
Valery A. Ilychev <ilychev@gmail.com>
2014-08-14 14:08:13 +0400

    Optimized geozones forms

--- rynda/geozones/forms.py
+++ rynda/geozones/forms.py
@@ -1,28 +1,10 @@
 # coding: utf-8
 
 from django import forms
-from django.utils.translation import ugettext_lazy as _
 
 from leaflet.forms.widgets import LeafletWidget
 
 from .models import Location
-from .widgets import GeolocationWidget
-
-
-class LocationField(forms.MultiValueField):
-
-    def __init__(self, *args, **kwargs):
-        self.widget = GeolocationWidget()
-        fields = (
-            forms.DecimalField(required=False, label=_('latitude')),
-            forms.DecimalField(required=False, label=_('longitude')),
-        )
-        super(LocationField, self).__init__(fields, required=False)
-
-    def compress(self, value_list):
-        if value_list:
-            return value_list
-        return ""
 
 
 class LocationForm(forms.ModelForm):
