c40a765b6b6d5553cb8d00dc69f75bcdeaa035bd
Valery A. Ilychev <ilychev@gmail.com>
2014-08-14 11:08:34 +0400

    PEP8 for backends

--- rynda/core/backends.py
+++ rynda/core/backends.py
@@ -34,26 +34,25 @@ class EmailAuthBackend(object):
 
 
 class IonAuth(object):
-    '''
-    Authentication against the codeigniter "ion auth" library
-    '''
+    """ Authentication against the codeigniter "ion auth" library """
+
     supports_inactive_user = False
     SALT_LENGTH = 10
 
     def get_user(self, user_id):
-        '''
-        Backend implementation
-        '''
+        """ Backend implementation """
+
         try:
             return User.objects.get(pk=user_id, is_active=True)
         except User.DoesNotExist:
             return None
 
     def authenticate(self, username=None, password=None):
-        '''
+        """
         Backend implementation.
+
         Implement the ion auth validation algorytm
-        '''
+        """
         if not username or not password:
             return None
         try:
@@ -62,28 +61,27 @@ class IonAuth(object):
             return None
         if not user.is_active:
             return None
-        check = self.password_hash(password,
+        check = self.password_hash(
+            password,
             salt=user.password[:self.SALT_LENGTH])
         if user.password != check:
             return None
-        #Rehash user password to standart Django 
+        # Rehash user password to standart Django
         user.set_password(password)
         user.save()
         return user
 
     def gen_salt(self):
-        '''
-        Generate password salt, similar to ion auth.
-        '''
+        """ Generate password salt, similar to ion auth.  """
+
         hasher = hashlib.md5()
         hasher.update("%f" % random())
-        hasher.update("%d" %time.time())
+        hasher.update("%d" % time.time())
         return hasher.hexdigest()[:self.SALT_LENGTH]
 
     def password_hash(self, password, salt=None):
-        '''
-        Hashes plain password by ion auth algorythm
-        '''
+        """ Hashes plain password by ion auth algorythm """
+
         if salt is None:
             salt = self.gen_salt()
         hasher = hashlib.sha1()
