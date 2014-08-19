dac44b0986ab065cbab0a52527e063fd6f5ea32b
Valery A. Ilychev <ilychev@gmail.com>
2014-08-19 10:38:16 +0400

    The local_stage settings is allowed

--- rynda/Rynda/settings/stage.py
+++ rynda/Rynda/settings/stage.py
@@ -20,3 +20,8 @@ STATIC_ROOT = os.path.join(get_env_var('STATIC_ROOT'), 'rynda', 'static')
 
 EXTERNAL = True
 VK_APP_ID = get_env_var('vk_app_id')
+
+try:
+    from local_stage import *
+except:
+    pass
