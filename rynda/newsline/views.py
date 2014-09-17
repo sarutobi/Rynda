6064eeb39b7d59e6dead6b5e80871098233b436b
Valery A. Ilychev <ilychev@gmail.com>
2014-09-17 16:43:45 +0400

    Renamed newsline templates according to name rules

--- rynda/newsline/views.py
+++ rynda/newsline/views.py
@@ -9,7 +9,7 @@ from .models import Post
 class NewsListView(RyndaListView):
     queryset = Post.objects.filter(
         status=Post.PUBLISHED, publish__lte=timezone.now())
-    template_name = "latest_post.html"
+    template_name = "post_list.html"
     context_object_name = "posts"
     paginator_url = "/news/?page="
     paginate_by = 10
