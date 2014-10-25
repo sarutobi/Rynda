2d9fff6af01a1b7a70b5deb439dd7418de0d6bcc
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2014-10-25 09:41:51 +0400

    Erased database settings from base settings

--- rynda/Rynda/settings/base.py
+++ rynda/Rynda/settings/base.py
@@ -20,17 +20,6 @@ SITE_ID = 1
 
 PRODUCTION = False
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        # 'NAME': '',                      # Or path to database file if using sqlite3.
-        # 'USER': '',                      # Not used with sqlite3.
-        # 'PASSWORD': '',                  # Not used with sqlite3.
-        # 'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    # }
-# }
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -43,8 +32,6 @@ TIME_ZONE = 'Europe/Moscow'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'ru'
-
-# SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
