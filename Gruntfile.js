4b531340da0ca6efa27f73d06cd434b98dcc9ae3
Valery A. Ilychev <ilychev@gmail.com>
2015-03-11 16:46:16 +0300

    Changed grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt){
 
         // Build css from less styles
         less: {
-            development: {
+            dev: {
                 files: {
                     "rynda/static/css/bootstrap-theme.css": "assets/css/rynda-theme.less",
                     "rynda/static/css/default.css": "assets/css/rynda.less"    
@@ -21,27 +21,30 @@ module.exports = function(grunt){
 
         // Copying files
         copy: {
-            // Libraries
-            libs: {
+            // Development version
+            dev: {
                 files: [
+                    // Libraries
                     {expand: true, cwd:"assets/javascript/", src:["libs/*.js"], dest: "rynda/static/js/"},
-                ]},
 
-            // Development javascripts
-            sourcejs: {
-                files:[
-                    {expand: true, cwd:"assets/javascript/", src:["*.js"], dest: "rynda/static/js/", filter: "isFile"} 
-                ]},
+                    // Development javascripts
+                    {expand: true, cwd:"assets/javascript/", src:["*.js"], dest: "rynda/static/js/", filter: "isFile"},
 
-            // Css files
-            css: {
-                files: [
+                    // Css files
                     {expand: true, cwd:"assets/css/", src:["*.css"], dest: "rynda/static/css/", filter: "isFile"} 
+                ]},
+
+            //Root files
+            prod: {
+                files:[
+                    {src:["*.txt", "README.md", "manage.py"], dest: "build/"},
+                    {src:["rynda/**/*.html", "rynda/**/*.json", "rynda/**/*.py" , "!**/tests/**", "!**/test/**"], dest:"build/"},
+                    {src:"locale/**/*.po", dest:"build/"}
                 ]}
         },
 
         // Clean destination dirs
-        clean: ["rynda/static/js", "rynda/static/css"]
+        clean: ["build", "rynda/static/js", "rynda/static/css"]
     });
 
     grunt.loadNpmTasks("grunt-contrib-concat");
@@ -49,6 +52,6 @@ module.exports = function(grunt){
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.registerTask("default", ["jshint", "copy:libs"]);
-    grunt.registerTask("develop", ["jshint", "clean", "less:development", "copy:libs", "copy:sourcejs", "copy:css"]);
+    grunt.registerTask("default", ["jshint", "clean", "less:dev", "copy:dev"]);
+    grunt.registerTask("package", ["jshint", "clean", "less:development", "copy:root", "copy:libs", "copy:sourcejs", "copy:css"]);
 };
